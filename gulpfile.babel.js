ba4b69e3f8ec40175e789436b113a5e94ca22ca0
Nils Schönwald <nils@schoenwald.media>
2017-05-06 16:40:56 +0200

    lint gulpfile, install critical

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,8 @@
 import gulp from 'gulp';
-const gulpRequireTasks = require('gulp-require-tasks');
-
-// SVG
+import gulpRequireTasks from 'gulp-require-tasks';
 import plumber from 'gulp-plumber';
 import svgSprite from 'gulp-svg-sprite';
+import { dirs, globs } from './gulp-tasks/paths';
 
 gulpRequireTasks();
 
@@ -13,20 +12,20 @@ gulp.task('build:svg-sprite', ['svgmin-color', 'svgmin-mono'], () => {
       symbol: {
         render: {
           css: {
-            template: '.svgSpriterc'
-          }
+            template: '.svgSpriterc',
+          },
         },
-        prefix: ".Svg--%s",
-        dimensions: "%s",
-        example: true
-      }
-    }
+        prefix: '.Svg--%s',
+        dimensions: '%s',
+        example: true,
+      },
+    },
   };
 
   return gulp.src(globs.svgCleaned)
     .pipe(plumber())
-    .pipe(svgSprite(config)).on('error', error => { console.log(error); })
-    .pipe(gulp.dest(dirs.dest + 'svg-sprite/'));
+    .pipe(svgSprite(config)).on('error', (error) => { console.log(error); })
+    .pipe(gulp.dest(`${dirs.dest}svg-sprite/`));
 });
 
 
