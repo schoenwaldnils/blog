2b0811e18ee6043871e3706b99738cde37558cca
Nils Schönwald <nils@schoenwald.media>
2017-10-04 00:42:14 +0200

    move encryption label

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,8 @@ fi
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 
-ENCRYPTED_KEY_VAR="encrypted_1d97a522c7ba_key"
-ENCRYPTED_IV_VAR="encrypted_1d97a522c7ba_iv"
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
