86fd8a375db17f1e58cda0c90e8050cf9895e06e
Nils Schönwald <nils@schoenwald.media>
2017-10-13 18:43:24 +0200

    switch to native custom-properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,16 @@
 const path = require('path');
 const webpackPostcssTools = require('webpack-postcss-tools');
 
-const map = webpackPostcssTools.makeVarMap(path.resolve(__dirname, 'source/css/theme.css'));
+const map = webpackPostcssTools.makeVarMap(path.resolve(__dirname, 'source/css/mediaQueries.css'));
 
 module.exports = () => ({
   plugins: {
     'postcss-import': {},
     'postcss-url': {},
     'postcss-nested': {},
-    'postcss-css-variables': {
-      variables: map.vars,
-    },
     'postcss-custom-media': {
       extensions: map.media,
     },
-    'postcss-color-function': {},
     'postcss-calc': {},
     'postcss-pseudoelements': {},
     autoprefixer: {},
