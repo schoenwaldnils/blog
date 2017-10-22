2911b5b50f52b44ddb0bd29763f0325b2c570fe7
Nils Schönwald <nils@schoenwald.media>
2017-10-22 08:14:00 +0200

    add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ export default class SmediaDocument extends Document {
   }
 
   render() {
+    const prod = process.env.NODE_ENV === 'production';
     return (
       <html lang="en">
         <Head>
@@ -25,6 +26,16 @@ export default class SmediaDocument extends Document {
           <meta name="theme-color" content="#ba3e48" />
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
           {this.props.styleTags}
+          {prod && <script dangerouslySetInnerHTML={{
+            __html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-33721620-2', 'auto');
+            ga('send', 'pageview');`,
+          }} />}
         </Head>
         <body>
           <Header />
