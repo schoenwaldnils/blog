01e5f165c88871349c4e1ffe53347dacf989d827
Nils Schönwald <nils@schoenwald.media>
2017-10-26 13:32:25 +0200

    add drift back to site

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,8 @@ export default class SmediaDocument extends Document {
           <Footer />
           <NextScript />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Karla" />
+
+          {/* Google Analytics codeblock */}
           {prod && <script dangerouslySetInnerHTML={{
             __html: `
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -40,6 +42,31 @@ export default class SmediaDocument extends Document {
 
             ga('create', 'UA-33721620-2', 'auto');
             ga('send', 'pageview');`,
+          }} />}
+
+          {/* Drift codeblock */}
+          {true && <script dangerouslySetInnerHTML={{
+            __html: `
+            !function() {
+              var t;
+              if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
+              t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+              t.factory = function(e) {
+                return function() {
+                  var n;
+                  return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
+                };
+              }, t.methods.forEach(function(e) {
+                t[e] = t.factory(e);
+              }), t.load = function(t) {
+                var e, n, o, i;
+                e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
+                o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
+                n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
+              });
+            }();
+            drift.SNIPPET_VERSION = '0.3.1';
+            drift.load('4r7cns5dxyfi');`,
           }} />}
         </body>
       </html>
