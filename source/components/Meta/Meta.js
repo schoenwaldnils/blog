510953632a267674ce7a319ab6e9d00109de7764
Nils Schönwald <nils@schoenwald.media>
2017-10-30 16:02:19 +0100

    fix meta

--- source/components/Meta/Meta.js
+++ source/components/Meta/Meta.js
@@ -23,7 +23,7 @@ const Meta = ({
   const alteredImage = image.includes('contentful') ? `https:${image}?w=200&h=200&fit=fill` : image;
   return (
     <Head>
-      <meta content="ie=edge" httpEquiv="x-ua-compatible" />
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
       <meta content="width=device-width,initial-scale=1" name="viewport" />
 
       <title>{ title }</title>
