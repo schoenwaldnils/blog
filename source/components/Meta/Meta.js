71702c32a8412ccc1f8950b918929079b9e9550d
Nils Schönwald <nils@schoenwald.media>
2017-10-30 04:07:25 +0100

    add fix for apple-icon without sizes

--- source/components/Meta/Meta.js
+++ source/components/Meta/Meta.js
@@ -5,7 +5,14 @@ import Parser from 'html-react-parser';
 import faviconData from '../../../faviconData.json';
 
 const faviconHtml = faviconData && faviconData.favicon.html_code;
-const Favicons = () => Parser(faviconHtml);
+const Favicons = () => Parser(faviconHtml, {
+  replace(domNode) {
+    // TODO: fix for https://sonarwhal.com/docs/user-guide/rules/apple-touch-icons/
+    if (domNode.attribs && domNode.attribs.rel === 'apple-touch-icon') {
+      return <link rel="apple-touch-icon" href="/static/assets/images/favicons/apple-touch-icon.png?v=1" />;
+    }
+  },
+});
 
 const Meta = ({
   url, type, title, description, image,
