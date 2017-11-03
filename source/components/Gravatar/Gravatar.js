d9c7bd0904a4582e1131bbbd598baf96bacef6aa
Nils Schönwald <nils@schoenwald.media>
2017-11-03 17:07:03 +0100

    move utils

--- source/components/Gravatar/Gravatar.js
+++ source/components/Gravatar/Gravatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import gravatarUrl from '../../js/utils';
+import gravatarUrl from '../../js/utils/gravatar-url';
 
 const Gravatar = ({ className, email, size }) => (
   <picture>
---
+++ source/js/utils/debounce.js
@@ -0,0 +1,17 @@
+const debounce = (func, wait, immediate) => {
+  let timeout;
+  return () => {
+    const context = this;
+    const args = arguments;
+    const later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+export default debounce;
