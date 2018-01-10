3011c79b59a147c3f25c1d1513ed61c6ca6ac01c
Nils Schönwald <nils@schoenwald.media>
2018-01-10 15:21:53 +0100

    hide edit button

--- pages/page.js
+++ pages/page.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { getFields } from '../scripts/contentful';
 import Meta from '../source/components/Meta/Meta';
 import Post from '../source/components/Post/Post';
-import Button from '../source/components/Button/Button';
+// import Button from '../source/components/Button/Button';
 import Disqus from '../source/components/Disqus/Disqus';
 
 const Page = ({ type, fields }) => [
