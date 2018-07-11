adca3e037325ae4769b91e881a66cfe067121d29
Nils Schönwald <nils@schoenwald.media>
2018-07-11 02:05:45 +0200

    edit branch name

--- scripts/manage-subdomain.sh
+++ scripts/manage-subdomain.sh
@@ -9,9 +9,9 @@ if [ $returnCode != 0 ]
 then
   plesk bin subdomain --create $BRANCH_NAME -domain $DOMAIN_NAME -ssl true -ssl-redirect true -empty-document-root true -www-root www/schoenwald/feature/$BRANCH_NAME
 
-  sleep 20s
+  # sleep 20s
 
-  plesk bin extension --exec letsencrypt cli.php -d $BRANCH_NAME.$DOMAIN_NAME -m nils@schoenwald.media
+  # plesk bin extension --exec letsencrypt cli.php -d $BRANCH_NAME.$DOMAIN_NAME -m nils@schoenwald.media
 else
   echo "Subdomain already exists"
   exit 0
