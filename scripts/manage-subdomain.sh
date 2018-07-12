51c3c6fb8323214aba3da96b44a68a2b45f47d37
Nils Schönwald <nils@schoenwald.media>
2018-07-12 03:28:06 +0200

    edit command again 19

--- scripts/manage-subdomain.sh
+++ scripts/manage-subdomain.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo BRANCH_NAME = $BRANCH_NAME
-echo DOMAIN_NAME = $DOMAIN_NAME
-ssh root@5.35.245.22 plesk bin subdomain -i $BRANCH_NAME.$DOMAIN_NAME
+echo BRANCH_NAME = ${BRANCH_NAME}
+echo DOMAIN_NAME = ${DOMAIN_NAME}
+ssh root@5.35.245.22 plesk bin subdomain -i ${BRANCH_NAME}.${DOMAIN_NAME}
 
 returnCode=$?
 
 if [ $returnCode != 0 ]
 then
-  ssh root@5.35.245.22 plesk bin subdomain --create $BRANCH_NAME -domain $DOMAIN_NAME -empty-document-root true -www-root www/schoenwald/$BRANCH_NAME
+  ssh root@5.35.245.22 plesk bin subdomain --create ${BRANCH_NAME} -domain ${DOMAIN_NAME} -empty-document-root true -www-root www/schoenwald/$BRANCH_NAME
 
   # sleep 20s
 
-  # ssh root@5.35.245.22 plesk bin extension --exec letsencrypt cli.php -d $BRANCH_NAME.$DOMAIN_NAME -m nils@schoenwald.media
+  # ssh root@5.35.245.22 plesk bin extension --exec letsencrypt cli.php -d ${BRANCH_NAME}.${DOMAIN_NAME} -m nils@schoenwald.media
 else
   echo "Subdomain already exists"
   exit 0
