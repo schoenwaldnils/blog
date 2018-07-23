81c920e542db48d7135e587e36138997d2a17e21
Nils Schönwald <nils@schoenwald.media>
2018-07-23 13:31:37 +0200

    fix quotes

--- scripts/manage-subdomain.sh
+++ scripts/manage-subdomain.sh
@@ -7,7 +7,7 @@ returnCode=$?
 
 if [ $returnCode != 0 ]
 then
-  ssh root@5.35.245.22 plesk bin subdomain --create ${BRANCH_NAME} -domain ${DOMAIN_NAME} -ssl true -ssl-redirect true -certificate-name "Lets Encrypt schoenwald.media" -empty-document-root true -www-root www/schoenwald/$BRANCH_NAME
+  ssh root@5.35.245.22 plesk bin subdomain --create ${BRANCH_NAME} -domain ${DOMAIN_NAME} -ssl true -ssl-redirect true -certificate-name 'Lets Encrypt schoenwald.media' -empty-document-root true -www-root www/schoenwald/$BRANCH_NAME
   # ssh root@5.35.245.22 plesk bin extension --exec letsencrypt cli.php -d ${BRANCH_NAME}.${DOMAIN_NAME} -m nils@schoenwald.media
 else
   echo "Subdomain already exists"
