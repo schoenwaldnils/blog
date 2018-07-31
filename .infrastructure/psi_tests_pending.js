22761305944c5af18d0edb5a38e11d2e03831d61
Nils Schönwald <nils@schoenwald.media>
2018-07-31 23:08:30 +0200

    install octonode

--- .infrastructure/psi_tests_pending.js
+++ .infrastructure/psi_tests_pending.js
@@ -1,6 +1,8 @@
-import fetch from 'isomorphic-fetch';
+import github from 'octonode';
 
 const {
+  BOT_GITHUB_TOKEN,
+  CIRCLE_SHA1,
   CIRCLE_PROJECT_USERNAME,
   CIRCLE_PROJECT_REPONAME,
   CIRCLE_PULL_REQUEST,
@@ -8,7 +10,7 @@ const {
 } = process.env;
 
 const REPO_SLUG = `${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}`;
-const CIRCLE_PR_NUMBER = CIRCLE_PULL_REQUEST.match(/[^/]+$/);
+const CIRCLE_PR_NUMBER = CIRCLE_PULL_REQUEST.match(/[^/]+$/) && CIRCLE_PULL_REQUEST.match(/[^/]+$/)[0];
 
 const BRANCH_NAME = CIRCLE_BRANCH.replace(/\//, '-');
 
@@ -19,26 +21,23 @@ console.log('CIRCLE_BRANCH', CIRCLE_BRANCH);
 console.log('BRANCH_NAME', BRANCH_NAME);
 console.log('CIRCLE_PR_NUMBER', CIRCLE_PR_NUMBER);
 
-// const testUrl = 'http://test.schoenwald.media/'; // FIXME: url static
-// const githubUrl = `https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${CIRCLE_SHA1}`;
+const testUrl = `https://${BRANCH_NAME}.schoenwald.media/`;
+const githubUrl = `https://api.github.com/repos/${REPO_SLUG}/statuses/${CIRCLE_SHA1}`;
 
-// // npm github api module?
 
-// fetch(
-//   githubUrl,
-//   {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: `token ${BOT_GITHUB_TOKEN}`,
-//     },
-//     body: {
-//       state: 'pending',
-//       description: 'PSI tests pending',
-//       context: 'test/psi/desktop/index',
-//     },
-//   },
-// );
+const client = github.client(BOT_GITHUB_TOKEN);
+const ghrepo = client.repo(REPO_SLUG);
+
+ghrepo.status(CIRCLE_SHA1, {
+  state: 'pending',
+  target_url: `https://developers.google.com/speed/pagespeed/insights/?url=${testUrl}&tab=mobile`,
+  description: 'PSI test mobile pending',
+}, () => console.log('Github status set \'PSI test mobile pending\'')); // created status
+
+ghrepo.status(CIRCLE_SHA1, {
+  state: 'pending',
+  target_url: `https://developers.google.com/speed/pagespeed/insights/?url=${testUrl}&tab=desktop`,
+  description: 'PSI test desktop pending',
+}, () => console.log('Github status set \'PSI test desktop pending\'')); // created status
 
 exit(0);
