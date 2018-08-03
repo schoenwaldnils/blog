b36c5bc57bf889087456879b004c20ead6ef2d70
Nils Schönwald <nils@schoenwald.media>
2018-08-03 15:28:23 +0200

    move from circleci to node-scipt

--- .infrastructure/psi_tests.js
+++ .infrastructure/psi_tests.js
@@ -1,58 +1,14 @@
-import github from 'octonode';
 import psi from 'psi';
-
-const {
-  BOT_GITHUB_TOKEN,
+import {
+  tests,
   CIRCLE_SHA1,
-  CIRCLE_PROJECT_USERNAME,
-  CIRCLE_PROJECT_REPONAME,
-  CIRCLE_BRANCH,
-} = process.env;
-
-const REPO_SLUG = `${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}`;
-
-const BRANCH_NAME = CIRCLE_BRANCH.replace(/\//, '-');
-
-const testUrl = `https://${BRANCH_NAME}.schoenwald.media/`;
+  testUrl,
+  ghrepo,
+  statusCallback,
+} from './psi_vars';
 
 
-const client = github.client(BOT_GITHUB_TOKEN);
-const ghrepo = client.repo(REPO_SLUG);
-
-const statusCallback = (err, message) => {
-  if (err) {
-    console.error(err.red);
-    process.exit(0);
-    return;
-  }
-
-  return console.log('\x1b[33m%s\x1b[0m', message);  //yellow
-}
-
-export const tests = [
-  {
-    environment: 'mobile',
-    test: 'speed',
-    min_expected_score: 85,
-  },
-  {
-    environment: 'mobile',
-    test: 'usability',
-    min_expected_score: 85,
-  },
-  {
-    environment: 'desktop',
-    test: 'speed',
-    min_expected_score: 85,
-  },
-  {
-    environment: 'desktop',
-    test: 'usability',
-    min_expected_score: 85,
-  },
-];
-
-tests.forEach(async ({ environment, test }) => {
+tests.forEach(async ({ environment }) => {
   const results = await psi(testUrl, {
     nokey: 'true',
     strategy: environment,
@@ -60,16 +16,17 @@ tests.forEach(async ({ environment, test }) => {
   });
   console.log(results);
   const { ruleGroups: {
-    SPEED: { score: speed_score },
-    USABILITY: { score: usability_score },
+    USABILITY: { score },
    } } = results;
 
-  const target_url = `https://developers.google.com/speed/pagespeed/insights/?url=${testUrl}&tab=${environment}`;
+  console.log('score: ', score );
 
-  ghrepo.status(CIRCLE_SHA1, {
-    state: 'pending',
-    target_url,
-    description: `PSI test \'${environment} ${test}\' pending`,
-    context: `psi/${environment}/${test}`,
-  }, (err) => statusCallback(err, `Github status set "PSI test \'${environment} ${test}\' pending"`)); // created status
+  // const target_url = `https://developers.google.com/speed/pagespeed/insights/?url=${testUrl}&tab=${environment}`;
+
+  // ghrepo.status(CIRCLE_SHA1, {
+  //   state: 'pending',
+  //   target_url,
+  //   description: 'Test pending',
+  //   context: `PSI ${environment}`,
+  // }, (err) => statusCallback(err, `Github status set "PSI test \'${environment}\' pending"`)); // created status
 });
--- .infrastructure/psi_tests_pending.js
+++ .infrastructure/psi_tests_pending.js
@@ -1,39 +1,16 @@
-import github from 'octonode';
-import { tests } from './psi_tests';
-
-const {
-  BOT_GITHUB_TOKEN,
+import {
+  tests,
   CIRCLE_SHA1,
-  CIRCLE_PROJECT_USERNAME,
-  CIRCLE_PROJECT_REPONAME,
-  CIRCLE_BRANCH,
-} = process.env;
+  testUrl,
+  ghrepo,
+  statusCallback,
+} from './psi_vars';
 
-const REPO_SLUG = `${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}`;
-
-const BRANCH_NAME = CIRCLE_BRANCH.replace(/\//, '-');
-
-const testUrl = `https://${BRANCH_NAME}.schoenwald.media/`;
-
-
-const client = github.client(BOT_GITHUB_TOKEN);
-const ghrepo = client.repo(REPO_SLUG);
-
-const statusCallback = (err, message) => {
-  if (err) {
-    console.error(err.red);
-    process.exit(0);
-    return;
-  }
-
-  return console.log('\x1b[33m%s\x1b[0m', message);  //yellow
-}
-
-tests.forEach(({ environment, test }) => {
+tests.forEach(({ environment }) => {
   ghrepo.status(CIRCLE_SHA1, {
     state: 'pending',
     target_url: `https://developers.google.com/speed/pagespeed/insights/?url=${testUrl}&tab=${environment}`,
-    description: `PSI test \'${environment} ${test}\' pending`,
-    context: `psi/${environment}/${test}`,
-  }, (err) => statusCallback(err, `Github status set "PSI test \'${environment} ${test}\' pending"`)); // created status
+    description: 'Test pending',
+    context: `PSI ${environment}`,
+  }, (err) => statusCallback(err, `Github status set "PSI test \'${environment}\' pending"`)); // created status
 });
