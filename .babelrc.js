15fe7467a3d13d3135cbf6312c5eb2871d23d448
Nils Schönwald <nils@schoenwald.media>
2018-08-28 20:30:32 +0200

    remove docker

--- .babelrc.js
+++ .babelrc.js
@@ -1,38 +1,12 @@
-const env = {
-  VERSION: require('./package').version,
-  'process.env.CONTENTFUL_SPACE': process.env.CONTENTFUL_SPACE,
-  'process.env.CONTENTFUL_TOKEN': process.env.CONTENTFUL_TOKEN,
-  'process.env.CONTENTFUL_PREVIEW_TOKEN': process.env.CONTENTFUL_PREVIEW_TOKEN,
-  'process.env.CONTENTFUL_MANAGEMENT_TOKEN': process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-}
-
 module.exports = {
-  env: {
-    development: {
-      presets: [
-        ["next/babel", {
-          "preset-env": {
-            "modules": "commonjs"
-          }
-        }]
-      ],
-      plugins: [
-        "inline-react-svg",
-        ['transform-define', env],
-      ]
-    },
-    production: {
-      presets: [
-        ["next/babel", {
-          "preset-env": {
-            "modules": "commonjs"
-          }
-        }]
-      ],
-      plugins: [
-        "inline-react-svg",
-        ['transform-define', env]
-      ]
-    }
-  }
+  presets: [
+    ["next/babel", {
+      "preset-env": {
+        "modules": "commonjs"
+      }
+    }]
+  ],
+  plugins: [
+    "inline-react-svg",
+  ]
 };
