8b7a99d4fcaf866a764f8d70729707b036ecb975
Nils Schönwald <nils@schoenwald.media>
2018-08-28 20:48:59 +0200

    fix env bla

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,21 @@
+const env = {
+  VERSION: require('./package').version,
+  'process.env.CONTENTFUL_SPACE': process.env.CONTENTFUL_SPACE,
+  'process.env.CONTENTFUL_TOKEN': process.env.CONTENTFUL_TOKEN,
+  'process.env.CONTENTFUL_PREVIEW_TOKEN': process.env.CONTENTFUL_PREVIEW_TOKEN,
+  'process.env.CONTENTFUL_MANAGEMENT_TOKEN': process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+}
+
 module.exports = {
   presets: [
-    ["next/babel", {
-      "preset-env": {
-        "modules": "commonjs"
+    ['next/babel', {
+      'preset-env': {
+        modules: 'commonjs',
       }
     }]
   ],
   plugins: [
-    "inline-react-svg",
+    'inline-react-svg',
+    ['transform-define', env],
   ]
 };
