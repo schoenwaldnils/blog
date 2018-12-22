a7a7ffcce1e4586a9a3b73576dc31678a0f457fb
Nils Schönwald <nils@schoenwald.media>
2018-12-22 05:18:01 +0100

    add bot name

--- _infrastructure/octokit.js
+++ _infrastructure/octokit.js
@@ -3,6 +3,7 @@ import rest from '@octokit/rest';
 const octokit = rest();
 
 const {
+  BOT_NAME,
   BOT_GITHUB_TOKEN,
   CIRCLE_PROJECT_USERNAME,
   CIRCLE_PROJECT_REPONAME,
@@ -12,6 +13,7 @@ if (!BOT_GITHUB_TOKEN) throw new Error('Environment variable <BOT_NAME> undefine
 
 octokit.authenticate({
   type: 'token',
+  username: BOT_NAME,
   token: BOT_GITHUB_TOKEN,
 });
 
