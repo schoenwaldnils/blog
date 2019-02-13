a0be72fea8f6e2bdde4414cb6a146f6802244ee4
Nils Schönwald <nils@schoenwald.media>
2019-02-13 12:13:08 +0100

    filter css order warnings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require('@zeit/next-css');
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const Dotenv = require('dotenv-webpack');
 
 const exportPathMap = require('./scripts/exportPathMap');
@@ -17,6 +18,15 @@ module.exports = withCSS({
 
   webpack: (config, { dev }) => {
     console.warn(`Enviroment: ${dev ? 'DEVELOPMENT' : 'PRODUCTION'}`);
+
+    // Silence mini-css-extract-plugin generating lots of warnings for CSS ordering.
+    // We use CSS modules that should not care for the order of CSS imports, so we
+    // should be safe to ignore these.
+    //
+    // See: https://github.com/webpack-contrib/mini-css-extract-plugin/issues/250
+    config.plugins.push(new FilterWarningsPlugin({
+      exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
+    }));
 
     if (dev) {
       config.plugins.push(new Dotenv());
