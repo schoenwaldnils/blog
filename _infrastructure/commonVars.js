2902eb1226f22c41b26c145326eb2e74d6487f81
Nils Schönwald <nils@schoenwald.media>
2019-02-14 14:06:12 +0100

    lebab common_vars

--- _infrastructure/commonVars.js
+++ _infrastructure/commonVars.js
@@ -7,8 +7,8 @@ if (!DOMAIN_NAME) throw new Error('Environment variable <DOMAIN_NAME> undefined!
 
 const branchName = CIRCLE_BRANCH && CIRCLE_BRANCH.replace(/[/|/.|_]/g, '-').replace(/@/g, '');
 
-export const previewUrl = branchName ? `https://${branchName}.${DOMAIN_NAME}/` : `https://${DOMAIN_NAME}/`;
+const previewUrl = branchName ? `https://${branchName}.${DOMAIN_NAME}/` : `https://${DOMAIN_NAME}/`;
 
-export default {
+module.exports = {
   previewUrl,
 };
