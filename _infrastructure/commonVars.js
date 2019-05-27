7a69e2bf597c03a039414a1939644a6c96ed023f
Nils Schönwald <nils@schoenwald.media>
2019-05-27 21:14:23 +0200

    test node export to env

--- _infrastructure/commonVars.js
+++ _infrastructure/commonVars.js
@@ -1,16 +1,11 @@
 const {
-  CIRCLE_BRANCH,
+  BRANCH_NAME,
   DOMAIN_NAME,
 } = process.env;
 
 if (!DOMAIN_NAME) throw new Error('Environment variable <DOMAIN_NAME> undefined!');
 
-const branchName = CIRCLE_BRANCH && CIRCLE_BRANCH
-  .replace('/npm_and_yarn', '')
-  .replace(/[/|/.|_]/g, '-')
-  .replace(/@/g, '');
-
-export const previewUrl = branchName ? `https://${branchName}.${DOMAIN_NAME}/` : `https://${DOMAIN_NAME}/`;
+export const previewUrl = BRANCH_NAME ? `https://${BRANCH_NAME}.${DOMAIN_NAME}/` : `https://${DOMAIN_NAME}/`;
 
 export default {
   previewUrl,
---
+++ _infrastructure/setBranchName.js
@@ -0,0 +1,10 @@
+const {
+  CIRCLE_BRANCH,
+} = process.env;
+
+const branchName = CIRCLE_BRANCH && CIRCLE_BRANCH
+  .replace('/npm_and_yarn', '')
+  .replace(/[/|/.|_]/g, '-')
+  .replace(/@/g, '');
+
+console.log(branchName);
